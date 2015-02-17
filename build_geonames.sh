dc91715a5a495dc9130ea5e854a55bdb2122c4d6
Matthew Perry <mperry@Matthews-MacBook-Pro.local>
2015-02-17 15:36:16 -0800

    initial alterations for my custom postgis geonames import

--- build_geonames.sh
+++ build_geonames.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ################################################################################
 #   ____    ____    _____                                                      #
 #  /\  _`\ /\  _`\ /\  __`\                                                    #
@@ -30,21 +31,21 @@
 #                                                                              #
 # Jack Varga <jack.varga at gmail dot com> Fri Dec  7 10:52:15 MST 2012        #
 ################################################################################
-#!/bin/bash
     
-WORKPATH="${HOME}/tmp/geonames"
+WORKPATH="./work" # local path
+VMWORKPATH="/work" # Should be absolute path ON THE VM not the local machine!
 TMPPATH="tmp"
 WORKDIR=${WORKPATH}/${TMPPATH}
 POSTALCODEPATH="pc"
 POSTALCODEDIR=${WORKPATH}/${POSTALCODEPATH}
 POSTALCODES=US.zip
 PREFIX="_"
-DBHOST="127.0.0.1"
+DBHOST="localhost"
 DBPORT="5432"
 DBNAME="geonames"
-TZ="America/Denver" ; export TZ
+TZ="America/Los Angeles"
 PGVERSION="9.3"
-PGISVERSION="2.1"
+PGISVERSION='2.1'
 DBUSER="postgres"
 GEOROLE="georole"
 GEOUSER="geouser"
@@ -57,8 +58,8 @@ FILES="allCountries.zip alternateNames.zip admin1CodesASCII.txt admin2Codes.txt 
 TABLES="admin1codes admin2codes alternatename continentcodes countryinfo featurecodes postalcodes geoname languagecodes timezones"
 
 echo -e "+----CREATE ${DBNAME} DATABASE (step 1 of 8)----------+\n"
-#psql -U $DBUSER -h $DBHOST -p $DBPORT <<EOT
-psql <<EOT
+
+psql -U $DBUSER -h $DBHOST -p $DBPORT <<EOT
 DROP DATABASE ${DBNAME}; 
 DROP ROLE IF EXISTS ${GEOROLE};
 DROP USER IF EXISTS ${GEOUSER};
@@ -66,13 +67,12 @@ DROP ROLE IF EXISTS ${DEVROLE};
 DROP USER IF EXISTS ${DEVUSER};
 EOT
 
-#psql -U $DBUSER -h $DBHOST -p $DBPORT -c "CREATE DATABASE ${DBNAME} WITH TEMPLATE = template0 ENCODING = 'UTF8';" 
-psql -c "CREATE DATABASE ${DBNAME} WITH TEMPLATE = template1 ENCODING = 'UTF8';" 
+psql -U $DBUSER -h $DBHOST -p $DBPORT -c \
+    "CREATE DATABASE ${DBNAME} WITH TEMPLATE = template0 ENCODING = 'UTF8';" 
 
 echo -e "\n+-----CREATE TABLES and SEQUENCES (step 2 of 8)----------+\n"
 
-#psql -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
-psql -d ${DBNAME} <<EOT
+psql -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
 
 DROP TABLE IF EXISTS geoname CASCADE;
 CREATE TABLE geoname (
@@ -86,7 +86,7 @@ CREATE TABLE geoname (
     fclass character(1),
     fcode varchar(10),
     country varchar(2),
-    cc2 varchar(60),
+    cc2 varchar(90), -- was 60
     admin1 varchar(20),
     admin2 varchar(80),
     admin3 varchar(20),
@@ -141,7 +141,7 @@ CREATE TABLE countryinfo (
 DROP TABLE IF EXISTS admin1codes;
 CREATE TABLE admin1codes (
     id SERIAL NOT NULL,
-    code character(10) NOT NULL,
+    code character(14) NOT NULL, -- was 10
     name text,
     nameascii text,
     geonameid integer,
@@ -274,17 +274,19 @@ unzip -o ${POSTALCODES} US.txt
 
 echo -e "\n+----POPULATE TABLES (step 4 of 8)----------+\n"
 
-#psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
-psql -e -d ${DBNAME} <<EOT
-copy geoname (geonameid,name,asciiname,alternatenames,latitude,longitude,fclass,fcode,country,cc2,admin1,admin2,admin3,admin4,population,elevation,gtopo30,timezone,moddate) from '${WORKPATH}/${TMPPATH}/allCountries.txt' null as '';
-copy postalcodes (countrycode,postalcode,placename,admin1name,admin1code,admin2name,admin2code,admin3name,admin3code,latitude,longitude,accuracy) from '${WORKPATH}/${POSTALCODEPATH}/US.txt' null as '';
-copy timezones (countrycode,timeZoneId,GMT_offset,DST_offset,raw_offset) from '${WORKPATH}/${TMPPATH}/timeZones.txt.tmp' null as '';
-copy featureCodes (code,name,description) from '${WORKPATH}/${TMPPATH}/featureCodes_en.txt' null as '';
-copy admin1codes (code,name,nameAscii,geonameid) from '${WORKPATH}/${TMPPATH}/admin1CodesASCII.txt' null as '';
-copy admin2codes (code,name,alternatename,geonameid) from '${WORKPATH}/${TMPPATH}/admin2Codes.txt' null as '';
-copy languagecodes (iso_639_3,iso_639_2,iso_639_1,language_name) from '${WORKPATH}/${TMPPATH}/iso-languagecodes.txt.tmp' null as '';
-copy countryInfo (country_code,iso3,iso_numeric,fips,country_name,capital,areainsqkm,population,continent,tld,currency_code,currency_name,phone,postal_code_fmt,postal_code_rgx,languages,geonameid,neighbors,equiv_fips_code) from '${WORKPATH}/${TMPPATH}/countryInfo.txt.tmp' null as '';
-copy alternatename (alternatenameid,geonameid,isoLanguage,alternateName,isPreferredName,isShortName,isColloquial,isHistoric) from '${WORKPATH}/${TMPPATH}/alternateNames.txt' null as '';
+psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
+copy geoname (geonameid,name,asciiname,alternatenames,latitude,longitude,fclass,fcode,country,cc2,admin1,admin2,admin3,admin4,population,elevation,gtopo30,timezone,moddate) from '${VMWORKPATH}/${TMPPATH}/allCountries.txt' null as '';
+EOT
+
+psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
+copy postalcodes (countrycode,postalcode,placename,admin1name,admin1code,admin2name,admin2code,admin3name,admin3code,latitude,longitude,accuracy) from '${VMWORKPATH}/${POSTALCODEPATH}/US.txt' null as '';
+copy timezones (countrycode,timeZoneId,GMT_offset,DST_offset,raw_offset) from '${VMWORKPATH}/${TMPPATH}/timeZones.txt.tmp' null as '';
+copy featureCodes (code,name,description) from '${VMWORKPATH}/${TMPPATH}/featureCodes_en.txt' null as '';
+copy admin1codes (code,name,nameAscii,geonameid) from '${VMWORKPATH}/${TMPPATH}/admin1CodesASCII.txt' null as '';
+copy admin2codes (code,name,alternatename,geonameid) from '${VMWORKPATH}/${TMPPATH}/admin2Codes.txt' null as '';
+copy languagecodes (iso_639_3,iso_639_2,iso_639_1,language_name) from '${VMWORKPATH}/${TMPPATH}/iso-languagecodes.txt.tmp' null as '';
+copy countryInfo (country_code,iso3,iso_numeric,fips,country_name,capital,areainsqkm,population,continent,tld,currency_code,currency_name,phone,postal_code_fmt,postal_code_rgx,languages,geonameid,neighbors,equiv_fips_code) from '${VMWORKPATH}/${TMPPATH}/countryInfo.txt.tmp' null as '';
+copy alternatename (alternatenameid,geonameid,isoLanguage,alternateName,isPreferredName,isShortName,isColloquial,isHistoric) from '${VMWORKPATH}/${TMPPATH}/alternateNames.txt' null as '';
 INSERT INTO continentCodes (code,name,geonameid) VALUES ('AF', 'Africa', 6255146);
 INSERT INTO continentCodes (code,name,geonameid) VALUES ('AS', 'Asia', 6255147);
 INSERT INTO continentCodes (code,name,geonameid) VALUES ('EU', 'Europe', 6255148);
@@ -296,8 +298,7 @@ EOT
 
 echo -e "\n+----CREATING INDEXES ON GEONAME IDS (step 5 of 8)---------+\n"
 
-#psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
-psql -e -d ${DBNAME} <<EOT
+psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
 CREATE INDEX idx_countryinfo ON countryinfo USING btree (geonameid);
 CREATE INDEX idx_alternatename ON alternatename USING btree (geonameid);
 CREATE INDEX idx_admin1codes ON admin1codes USING btree (geonameid);
@@ -306,21 +307,33 @@ CREATE INDEX idx_admin2codes ON admin2codes USING btree (geonameid);
 CREATE INDEX idx_admin2codes_name ON admin2codes USING btree (name);
 EOT
 
+echo -e "\n+----CREATING INDEXES ON featurecodes (step 5.5 of 8)---------+\n"
+
+psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
+ALTER TABLE geoname ADD COLUMN featurecodeid varchar(11);
+UPDATE geoname SET featurecodeid = fclass || '.' || fcode;
+CREATE INDEX idx_featurecodeid ON geoname USING btree (featurecodeid);
+CREATE INDEX idx_code ON featurecodes USING btree (code);
+EOT
+
+
 echo -e "\n+----CREATING SPATIAL GEOMETRIES (step 6 of 8)----------+\n"
 
 # Add postgis spatial features to new database
 # Helps to verify topology feature location on disk
-createlang plpgsql ${DBNAME}
-psql -e -d ${DBNAME} -f ${POSTGISPATH}/postgis.sql
-psql -e -d ${DBNAME} -f ${POSTGISPATH}/postgis_comments.sql
-psql -e -d ${DBNAME} -f ${POSTGISPATH}/spatial_ref_sys.sql
-psql -e -d ${DBNAME} -f ${POSTGISPATH}/rtpostgis.sql
-psql -e -d ${DBNAME} -f ${POSTGISPATH}/raster_comments.sql
-psql -e -d ${DBNAME} -f ${POSTGISPATH}/topology.sql
-psql -e -d ${DBNAME} -f ${POSTGISPATH}/topology_comments.sql
+psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
+  CREATE OR REPLACE LANGUAGE plpgsql;
+  CREATE EXTENSION postgis;
+EOT
+# psql -e -d ${DBNAME} -f ${POSTGISPATH}/postgis.sql
+# psql -e -d ${DBNAME} -f ${POSTGISPATH}/postgis_comments.sql
+# psql -e -d ${DBNAME} -f ${POSTGISPATH}/spatial_ref_sys.sql
+# psql -e -d ${DBNAME} -f ${POSTGISPATH}/rtpostgis.sql
+# psql -e -d ${DBNAME} -f ${POSTGISPATH}/raster_comments.sql
+# psql -e -d ${DBNAME} -f ${POSTGISPATH}/topology.sql
+# psql -e -d ${DBNAME} -f ${POSTGISPATH}/topology_comments.sql
 
-#psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
-psql -e -d ${DBNAME} <<EOT
+psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
 SELECT AddGeometryColumn ('public','geoname','the_geom',4326,'POINT',2);
 UPDATE geoname SET the_geom = ST_PointFromText('POINT(' || longitude || ' ' || latitude || ')', 4326);
 --UPDATE geoname SET the_geom = ST_SetSRID(ST_Point(longitude,latitude),4326);
@@ -332,8 +345,7 @@ EOT
 
 echo -e "+----INDEX and CLUSTER GEOMETRIES (step 7 of 8)\n"
 
-#psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
-psql -e -d ${DBNAME} <<EOT
+psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
 CREATE INDEX idx_geoname ON geoname USING gist(the_geom);
 ALTER TABLE geoname ALTER COLUMN the_geom SET not null;
 CLUSTER idx_geoname ON geoname;
@@ -342,36 +354,36 @@ ALTER TABLE postalcodes ALTER COLUMN the_geom SET not null;
 CLUSTER idx_postalcodes ON postalcodes;
 EOT
 
-echo -e "\n\n+----CREATE ROLES and GRANT privileges to those who need READ or WRITE"
-echo -e "+----permissions (step 8 of 8)----------+\n\n"
+# echo -e "\n\n+----CREATE ROLES and GRANT privileges to those who need READ or WRITE"
+# echo -e "+----permissions (step 8 of 8)----------+\n\n"
 
-echo -e "++++ FIRST create read-only USERS +++\n"
+# echo -e "++++ FIRST create read-only USERS +++\n"
 
-psql -e -d ${DBNAME} <<EOT
-ALTER SCHEMA public RENAME TO ${DBNAME};
-CREATE USER ${GEOUSER} WITH login nocreaterole nocreateuser nocreatedb UNENCRYPTED PASSWORD '${GEOPASSWORD}';
-GRANT SELECT ON ALL TABLES IN SCHEMA ${DBNAME} TO ${GEOUSER};
-CREATE ROLE ${GEOROLE} INHERIT;
-GRANT SELECT ON ${DBNAME}.geometry_columns TO ${GEOROLE};
-GRANT SELECT ON ${DBNAME}.geography_columns TO ${GEOROLE};
-GRANT SELECT ON ${DBNAME}.spatial_ref_sys TO ${GEOROLE};
-GRANT ${GEOROLE} TO ${GEOUSER};
-EOT
+# psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
+# ALTER SCHEMA public RENAME TO ${DBNAME};
+# CREATE USER ${GEOUSER} WITH login nocreaterole nocreateuser nocreatedb UNENCRYPTED PASSWORD '${GEOPASSWORD}';
+# GRANT SELECT ON ALL TABLES IN SCHEMA ${DBNAME} TO ${GEOUSER};
+# CREATE ROLE ${GEOROLE} INHERIT;
+# GRANT SELECT ON ${DBNAME}.geometry_columns TO ${GEOROLE};
+# GRANT SELECT ON ${DBNAME}.geography_columns TO ${GEOROLE};
+# GRANT SELECT ON ${DBNAME}.spatial_ref_sys TO ${GEOROLE};
+# GRANT ${GEOROLE} TO ${GEOUSER};
+# EOT
 
-echo -e "\n\n+----SECOND create user with WRITE privilges (INSERT, UPDATE, DELETE).\n"
+# echo -e "\n\n+----SECOND create user with WRITE privilges (INSERT, UPDATE, DELETE).\n"
 
-psql -e -d ${DBNAME} <<EOT
-CREATE USER ${DEVUSER} WITH login nocreatedb nocreaterole nocreateuser UNENCRYPTED PASSWORD '${DEVPASSWORD}';
-GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA ${DBNAME} TO ${DEVUSER};
-CREATE ROLE ${DEVROLE} INHERIT;
-GRANT ${DEVROLE} TO ${DEVUSER};
-GRANT SELECT,INSERT,UPDATE,DELETE ON ${DBNAME}.geometry_columns TO ${DEVROLE};
-GRANT SELECT,INSERT,UPDATE,DELETE ON ${DBNAME}.geography_columns TO ${DEVROLE};
-GRANT SELECT,INSERT,UPDATE,DELETE ON ${DBNAME}.spatial_ref_sys TO ${DEVROLE};
-EOT
+# psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
+# CREATE USER ${DEVUSER} WITH login nocreatedb nocreaterole nocreateuser UNENCRYPTED PASSWORD '${DEVPASSWORD}';
+# GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA ${DBNAME} TO ${DEVUSER};
+# CREATE ROLE ${DEVROLE} INHERIT;
+# GRANT ${DEVROLE} TO ${DEVUSER};
+# GRANT SELECT,INSERT,UPDATE,DELETE ON ${DBNAME}.geometry_columns TO ${DEVROLE};
+# GRANT SELECT,INSERT,UPDATE,DELETE ON ${DBNAME}.geography_columns TO ${DEVROLE};
+# GRANT SELECT,INSERT,UPDATE,DELETE ON ${DBNAME}.spatial_ref_sys TO ${DEVROLE};
+# EOT
 
-echo -e "\n\n+----FINISH by changing ownership of all tables, sequences and views"
-echo -e "+----to the database developer role -i.e. ${DEVROLE}.\n"
+# echo -e "\n\n+----FINISH by changing ownership of all tables, sequences and views"
+# echo -e "+----to the database developer role -i.e. ${DEVROLE}.\n"
 
 # NOTE:[EXPERIMENTAL] Not sure this is necessary or even a good 
 # idea, -i.e., change owner of all tables, sequences, views 
@@ -380,49 +392,49 @@ echo -e "+----to the database developer role -i.e. ${DEVROLE}.\n"
 # owner and just grant all proper read-write permissions with 
 # roles. Leave all functions and triggers owned by SUPERUSER 
 # (i.e. postgres).
-psql -e -d ${DBNAME} <<EOT
-ALTER TABLE ${DBNAME}.alternatename OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.countryinfo OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.continentcodes OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.languagecodes OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.admin1codes OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.geoname OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.spatial_ref_sys OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.postalcodes OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.admin2codes OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.featurecodes OWNER TO ${DEVUSER};
-ALTER TABLE ${DBNAME}.timezones OWNER TO ${DEVUSER};
-ALTER VIEW ${DBNAME}.geography_columns OWNER TO ${DEVUSER};
-ALTER VIEW ${DBNAME}.geometry_columns OWNER TO ${DEVUSER};
-ALTER VIEW ${DBNAME}.raster_columns OWNER TO ${DEVUSER};
-ALTER VIEW ${DBNAME}.raster_overviews OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.alternatename_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.countryinfo_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.continentcodes_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.languagecodes_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.admin1codes_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.geoname_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.postalcodes_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.admin2codes_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.featurecodes_id_seq OWNER TO ${DEVUSER};
-ALTER SEQUENCE ${DBNAME}.timezones_id_seq OWNER TO ${DEVUSER};
-ALTER TABLE topology.layer OWNER TO ${DEVUSER};
-ALTER TABLE topology.topology OWNER TO ${DEVUSER};
-ALTER SEQUENCE topology.topology_id_seq OWNER TO ${DEVUSER};
-ALTER SCHEMA ${DBNAME} OWNER TO ${DEVUSER};
-ALTER SCHEMA topology OWNER TO ${DEVUSER};
-ALTER DATABASE geonames OWNER TO ${DEVUSER};
-ALTER USER ${DEVUSER} SET search_path = '$user','geonames';
-ALTER USER ${GEOUSER} SET search_path = '$user','geonames';
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ${DBNAME} TO ${DEVROLE};
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ${DBNAME} TO ${GEOROLE};
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO ${DEVROLE};
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO ${GEOROLE};
-EOT
+# psql -e -U $DBUSER -h $DBHOST -p $DBPORT ${DBNAME} <<EOT
+# ALTER TABLE ${DBNAME}.alternatename OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.countryinfo OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.continentcodes OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.languagecodes OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.admin1codes OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.geoname OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.spatial_ref_sys OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.postalcodes OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.admin2codes OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.featurecodes OWNER TO ${DEVUSER};
+# ALTER TABLE ${DBNAME}.timezones OWNER TO ${DEVUSER};
+# ALTER VIEW ${DBNAME}.geography_columns OWNER TO ${DEVUSER};
+# ALTER VIEW ${DBNAME}.geometry_columns OWNER TO ${DEVUSER};
+# ALTER VIEW ${DBNAME}.raster_columns OWNER TO ${DEVUSER};
+# ALTER VIEW ${DBNAME}.raster_overviews OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.alternatename_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.countryinfo_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.continentcodes_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.languagecodes_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.admin1codes_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.geoname_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.postalcodes_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.admin2codes_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.featurecodes_id_seq OWNER TO ${DEVUSER};
+# ALTER SEQUENCE ${DBNAME}.timezones_id_seq OWNER TO ${DEVUSER};
+# --ALTER TABLE topology.layer OWNER TO ${DEVUSER};
+# --ALTER TABLE topology.topology OWNER TO ${DEVUSER};
+# ALTER SEQUENCE topology.topology_id_seq OWNER TO ${DEVUSER};
+# ALTER SCHEMA ${DBNAME} OWNER TO ${DEVUSER};
+# ALTER SCHEMA topology OWNER TO ${DEVUSER};
+# ALTER DATABASE geonames OWNER TO ${DEVUSER};
+# ALTER USER ${DEVUSER} SET search_path = '$user','geonames';
+# ALTER USER ${GEOUSER} SET search_path = '$user','geonames';
+# GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ${DBNAME} TO ${DEVROLE};
+# GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ${DBNAME} TO ${GEOROLE};
+# -- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO ${DEVROLE};
+# -- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO ${GEOROLE};
+# EOT
 
 sleep 1
-echo -e "\n\nIMPORTANT: Make sure to configure pg_hba.conf (and pb_ident.conf if using "
-echo -e "user maps) to give ${GEOUSER} the access permissions it requires and reload "
-echo -e "configuration - i.e., (\$ sudo /etc/init.d/postgresql reload).\n"
+# echo -e "\n\nIMPORTANT: Make sure to configure pg_hba.conf (and pb_ident.conf if using "
+# echo -e "user maps) to give ${GEOUSER} the access permissions it requires and reload "
+# echo -e "configuration - i.e., (\$ sudo /etc/init.d/postgresql reload).\n"
 echo -e "\n+----PROCESS COMPLETE.-----------------------+\n"
 exit 0
